925599d78f3d7b2070022bd36e5423a0452b0dbd
Martin Hrabovcin <mhrabovcin.c@mesosphere.com>
2017-07-07 14:21:57 +0200

    tls: Basic integration test for TLS

--- frameworks/helloworld/tests/test_tls.py
+++ frameworks/helloworld/tests/test_tls.py
@@ -1,12 +1,21 @@
 import pytest
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.x509.oid import NameOID
+from cryptography.x509.oid import ExtensionOID
 
 import sdk_cmd as cmd
 import sdk_install as install
 import sdk_plan as plan
+import sdk_tasks as tasks
 import sdk_utils
+import shakedown
 from tests.config import (
     PACKAGE_NAME
 )
+
+
+DEFAULT_BACKEND = default_backend()
 
 
 def setup_module(module):
@@ -15,7 +24,10 @@ def setup_module(module):
 
 
 def teardown_module(module):
+    # TODO(mh): Improve me
+    create_service_account('hello-world')
     install.uninstall(PACKAGE_NAME)
+    delete_service_account('hello-world')
 
 
 def create_service_account(name, secret_name=None):
@@ -79,11 +91,6 @@ def service_account():
     delete_service_account(name)
 
 
-@pytest.fixture()
-def cleanup_service_leftover():
-    install.uninstall(PACKAGE_NAME)
-
-
 def test_tls_basics(service_account):
     install.install(
         PACKAGE_NAME,
@@ -105,4 +112,103 @@ def test_tls_basics(service_account):
     # launch will fail if secrets are not available or not accessible
     plan.wait_for_completed_deployment(PACKAGE_NAME)
 
+    task_id = tasks.get_task_ids(PACKAGE_NAME, "tls")[0]
+    assert task_id
+
+    # Convert base64 encoded files to binary
+    _base64_decode_keystore(task_id, 'keystore.keystore', 'keystore.jks')
+    _base64_decode_keystore(task_id, 'keystore.truststore', 'truststore.jks')
+
+    # Load end-entity certificate from keystore and root CA cert from truststore
+    end_entity_cert = _export_cert_from_task_keystore(
+        task_id, 'keystore.jks', 'cert')
+
+    root_ca_cert_in_truststore = _export_cert_from_task_keystore(
+        task_id, 'truststore.jks', 'dcos-root')
+
+    # Check that certificate subject maches the service name
+    common_name = end_entity_cert.subject.get_attributes_for_oid(
+        NameOID.COMMON_NAME)[0].value
+    assert common_name == PACKAGE_NAME
+
+    sans = end_entity_cert.extensions.get_extension_for_oid(
+        ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+
+    cluster_root_ca_cert = x509.load_pem_x509_certificate(
+        cmd.request(
+            'get', shakedown.dcos_url_path('/ca/dcos-ca.crt')).content,
+        DEFAULT_BACKEND)
+
+    assert root_ca_cert_in_truststore.signature == cluster_root_ca_cert.signature
+
     install.uninstall(PACKAGE_NAME)
+
+
+def task_exec(task_name, command):
+    return cmd.run_cli(
+        "task exec {} {}".format(task_name, command))
+
+
+def _base64_decode_keystore(task, source_path, dest_path):
+    return task_exec(task,
+        "bash -c 'cat {source_path} | base64 -d > {dest_path}'".format(
+            source_path=source_path,
+            dest_path=dest_path
+        )
+    )
+
+
+def _export_cert_from_task_keystore(
+        task, keystore_path, alias):
+    cert_bytes = task_exec(
+        task, _keystore_export_command(keystore_path, alias, '-rfc')
+    ).encode('ascii')
+
+    return x509.load_pem_x509_certificate(
+        cert_bytes, DEFAULT_BACKEND)
+
+
+def _keystore_list_command(keystore_path, args=None):
+    """
+    Creates a command that can be executed using `dcos exec` CLI and will
+    list certificates from provided keystore using java `keytool` command.
+
+    https://docs.oracle.com/javase/8/docs/technotes/tools/windows/keytool.html
+
+    Args:
+        keystore_path (str): Path to the keystore file
+        args (str): Optionally addiontal arguments for the `keytool -list`
+            command.
+
+    Returns:
+        A string that can be used as `dcos exec` argument.
+    """
+    return _java_command(
+        'keytool -list -keystore {keystore_path} '
+        '-noprompt -storepass "" {args}'.format(
+            keystore_path=keystore_path,
+            args=args
+        )
+    )
+
+
+def _keystore_export_command(keystore_path, cert_alias, args=None):
+    return _java_command(
+        'keytool -exportcert -keystore {keystore_path} '
+        '-storepass "" -alias {alias} {args}'.format(
+            keystore_path=keystore_path,
+            alias=cert_alias,
+            args=args
+        )
+    )
+
+
+def _java_command(command):
+    return (
+        "bash -c ' "
+        "export JAVA_HOME=$(ls -d $MESOS_SANDBOX/jre*/); "
+        "export JAVA_HOME=${{JAVA_HOME%/}}; "
+        "export PATH=$(ls -d $JAVA_HOME/bin):$PATH; "
+        "{command}"
+        "'"
+    ).format(command=command)
